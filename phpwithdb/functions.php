fedf395046d73ed9e8618f5d5c6500838c7f5d21
Артем <insidia93@gmail.com>
2017-01-22 00:46:43 +0200

    вспомогательный файл в котором описаны функции подключения к бд,записи в бд,извлечения хранимых данных в бд.

---
+++ phpwithdb/functions.php
@@ -0,0 +1,18 @@
+﻿<?php
+
+function connectToDB(){
+  return  mysqli_connect('localhost','root','','test');
+}
+
+function insertIntoDB($resource,$sql){
+   return mysqli_query($resource,$sql);
+}
+
+function readFromDB($resource,$sql){
+    $resultat=mysqli_query($resource,$sql);
+    $tmp=[];
+  while($row = mysqli_fetch_array($resultat)){
+      $tmp[]=$row;
+  }
+return $tmp;
+}
