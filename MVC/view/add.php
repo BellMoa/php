d2b093d54770ab07c9f664019b4ee9c06ccbe6a0
Артем <insidia93@gmail.com>
2017-01-24 16:02:12 +0200

    Файл из категории внешнего вида в котором созданы поля для загрузки изображения и присвоения ему имени

---
+++ MVC/view/add.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<form action="add.php" method="post" enctype="multipart/form-data">
+    <label for="title">Название</label>
+    <input type="text" id="title" name="title">
+    <br>
+    <label for="image">Файл</label>
+    <input type="file" id="image" name="image">
+    <br>
+    <input type="submit">
+</form>
+
+</table>
+<p><a href="/index.php" >Добавить фото</a></p>
+</body>
+</html>
